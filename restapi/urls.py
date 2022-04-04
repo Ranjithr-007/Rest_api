be38f24e4ea4d0c7106d6b58adc84e2c0bd96de0
Ranjith <ranjithranjukpm333@gmail.com>
2022-04-04 11:07:33 +0530

    initial commit

--- restapi/urls.py
+++ restapi/urls.py
@@ -13,25 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, re_path, include
 from api import views
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_swagger.views import get_swagger_view  # <-- Here
-schema_view = get_swagger_view(title='Jaseci API')
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
-
 schema_view = get_schema_view(
     openapi.Info(
-        title="Jaseci API",
-        default_version='v1',
-        description="Welcome to the world of Jaseci",
-        terms_of_service="https://www.jaseci.org",
-        contact=openapi.Contact(email="jason@jaseci.org"),
-        license=openapi.License(name="Awesome IP"),
+        title="Izadosolutions",
+        default_version='',
+        description="Welcome to the world of Izado",
+        terms_of_service="https://wizardengineering.co/",
+        
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -44,7 +40,7 @@ urlpatterns = [
          name='schema-swagger-ui'),  #<-- Here
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
          name='schema-redoc'), 
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     path('register',views.register.as_view(),name='register'),
     path('login/',obtain_auth_token,name="login"),
     path('welcome',views.welcome.as_view(),name="welcome"),
